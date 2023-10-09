d6fb0df131f84bfa7eb17d0ca820a9d80c0205ff
Ubuntu <ubuntu@ip-172-31-20-109.us-west-2.compute.internal>
2023-10-09 03:09:06 +0000

    code cleanup

--- googleAdapter.py
+++ googleAdapter.py
@@ -6,9 +6,6 @@ import json
 
 class googleAdapter:
 
-#project_id='chiefofstaff-interview-test'
-#auth_provider_x509_cert_url='https://www.googleapis.com/oauth2/v1/certs'
-
     _CLIENT_ID = '698626242388-1cb2m9tkn0qmnhli127ei2v9ech1cndk.apps.googleusercontent.com'
     _CLIENT_SECRET = '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'
     _AUTH_URL = 'https://accounts.google.com/o/oauth2/auth'
@@ -20,7 +17,6 @@ class googleAdapter:
     _TOKEN_EXCHANGE_SUCCESS_ROUTE = '/google/token/success'
     _MAIL_API_URL = 'https://www.googleapis.com/gmail/v1/users/me/messages'
     _SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
-
 
 
     def get_provider_data(self):
@@ -36,10 +32,9 @@ class googleAdapter:
                     'client_kwargs': {'scope': ' '.join(self._SCOPES)}
                 }
 
-    def __init__(self, oauthConnector):#, session):
+    def __init__(self, oauthConnector):
         self.name = "google"
         self.oauth = oauthConnector
-        #self.session = session
         self.oauth.register(name = self.name, **self.get_provider_data())
 
     def authorize(self, email_addr = None):
@@ -47,26 +42,20 @@ class googleAdapter:
         auth_params = {'login_hint': email_addr}
         return self.oauth.google.authorize_redirect(redirect_uri, **auth_params)
     
-    SUCCESS_ROUTE = '/success'
-    #success_page = app_url + SUCCESS_ROUTE
-    
     def success(self):
-        #self.session.clear()
         return 'success'
     
-    def google_oauth_success(self):
+    def oauth_success(self):
         token = self.oauth.google.authorize_access_token()
         email = session.get('email_addr')
+        session['token'] = token
         print(email)
-        if token is not None:
-            insert_user(email, 1, json.dumps(token))
-        print(token)
-        return app.redirect(url_for(success, _external=True))
     
     def search_emails(self, since):
-        #since = request.args.get('since')
-        #query_string = f'maxResults=1&q=after:{since}'
         query = f'after:{since}'
-        resp = self.oauth.get(self._MAIL_API_URL, params={'q': query, 'maxResults': 1})
+        resp = self.oauth.google.get(self._MAIL_API_URL, params={'q': query, 'maxResults': 1}, token = session['token'])
+        messages = resp.json().get('messages', [])
+        print(messages)
+        return str(len(messages) > 0)
 
     
--- server.py
+++ server.py
@@ -18,41 +18,17 @@ oauth = OAuth(app)
 
 app_url = 'https://hallowelt.uk'
 
-GOOGLE_CLIENT_SECRETS_LOCATION = 'gmail_client_secret.json'
-GOOGLE_SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+SUCCESS_ROUTE = '/success'
+
 GOOGLE_OAUTH_SUCCESS_ROUTE = '/google/oauth/success'
 GOOGLE_TOKEN_EXCHANGE_SUCCESS_ROUTE = '/google/token/success'
 
-
 MICROSOFT_OAUTH_SUCCESS_ROUTE = '/microsoft/oauth/success'
+MICROSOFT_TOKEN_EXCHANGE_SUCCESS_ROUTE = '/microsoft/token/success'
 
 adapters = {
-        'gmail.com': googleAdapter(oauth)#, session)
+            'gmail.com': googleAdapter(oauth)
         }
-
-google_client_json = None
-with open(GOOGLE_CLIENT_SECRETS_LOCATION, 'rb') as f:
-    google_client_json = json.loads(f.read())
-if 'web' in google_client_json:
-    google_client_json = google_client_json['web']
-
-OAUTH_PROVIDERS = {
-    'google': {
-        'client_id':google_client_json['client_id'],
-        'client_secret':google_client_json['client_secret'],
-        'authorize_url':google_client_json['auth_uri'],
-        'authorize_params':{'access_type': 'offline'},
-        'access_token_url':google_client_json['token_uri'],
-        'access_token_params':None,
-        'refresh_token_url':google_client_json['token_uri'],
-        'redirect_to':GOOGLE_OAUTH_SUCCESS_ROUTE,
-        'client_kwargs':{'scope': ' '.join(GOOGLE_SCOPES), 'code_challenge_method': 'S256'}
-    },
-}
-
-for provider_name, provider_data in OAUTH_PROVIDERS.items():
-    oauth.register(name=provider_name, **provider_data)
-
 
 
 def microsoft_oauth(email_addr = None):
@@ -60,37 +36,6 @@ def microsoft_oauth(email_addr = None):
     auth_params = {}
     return oauth.microsoft.authorize_redirect(redirect_uri, **auth_params)
 
-def google_oauth(email_addr = None):
-    redirect_uri = url_for('google_oauth_success', _external=True)
-    auth_params = {'login_hint': email_addr}
-    return oauth.google.authorize_redirect(redirect_uri, **auth_params)
-
-def insert_user(email, authorized, token, last_authenticated):
-    #***add code to handle case where user already exists***
-    db = cosDB()
-    db.insert_user(email, authorized, token, last_authenticated)
-
-def refresh_user_token(email, token):
-    if 'refresh_token' not in token:
-        return None
-    refresh_token = token['refresh_token']
-    return None
-
-def get_user_token(email):
-    db = cosDB()
-    user = db.get_user(email)
-    if user is None:
-        return None
-    token = user['token']
-    if token is None or len(token) < 1:
-        return None
-    token = json.loads(token)
-    curr_time = int(time.time())
-    if 'expires_at' in token:
-        expires_at = token['expires_at']
-        if expires_at < curr_time:
-            return refresh_user_token(user, token)
-    return token
 
 @app.route('/connect/email')
 def connect_email():
@@ -99,105 +44,41 @@ def connect_email():
         abort(400) #add better indicator
 
     email_addr_parts = email_addr.split('@')
+
     if len(email_addr_parts) != 2:
         abort(400)
 
     email_domain = email_addr_parts[1]
-    valid_domains = ['microsoft.com', 'gmail.com']
-    auth_funcs = {
-        'microsoft.com': microsoft_oauth,
-        'gmail.com': google_oauth
-    }
-    #if email_domain not in valid_domains:
-    if email_domain not in auth_funcs:
+
+    if email_domain not in adapters:
         abort(400)
 
     session['email_addr'] = email_addr
+    session['domain'] = email_domain
 
-    print(auth_funcs[email_domain])
-    #return auth_funcs[email_domain](email_addr = email_addr)
     return adapters[email_domain].authorize(email_addr = email_addr)
 
-SUCCESS_ROUTE = '/success'
-success_page = app_url + SUCCESS_ROUTE
 
 @app.route(SUCCESS_ROUTE)
 def success():
-    #session.clear()
     return "success"
 
 @app.route(GOOGLE_OAUTH_SUCCESS_ROUTE)
 def google_oauth_success():
-    token = oauth.google.authorize_access_token()
-    email = session.get('email_addr')
-    print(email)
-    if token is not None:
-        insert_user(email, 1, json.dumps(token), int(time.time()))
-    print(token)
-    return app.redirect(success_page)
+    domain = 'gmail.com'
+    adapters[domain].oauth_success()
+    return app.redirect(url_for('success'))
 
 @app.route(MICROSOFT_OAUTH_SUCCESS_ROUTE)
 def microsoft_oauth_success():
-    token = oauth.microsoft.authorize_access_token()
-    email = session.get('email_addr')
-    print(email)
-    if token is not None:
-        insert_user(email, 1, json.dumps(token), int(time.time()))
-    print(token)
-    return app.redirect(success_page)
-
-def search_google_emails():
-    url = "https://www.googleapis.com/gmail/v1/users/me/messages"
-
-    # Your access token from the OAuth process
-    access_token = "YOUR_ACCESS_TOKEN"
-    
-    # Headers for authentication
-    headers = {
-        "Authorization": f"Bearer {access_token}"
-    }
-    
-    # Parameters for the search query
-    params = {
-        "q": "after:2023-10-05"  # Adjust the date as needed
-    }
-    
-    # Make the GET request
-    response = requests.get(url, headers=headers, params=params)
-    
-    # Parse the JSON response
-    messages = response.json().get('messages', [])
+    adapters['microsoft.com'].oauth_success()
+    return app.redirect(url_for('success'))
 
 @app.route('/emails/search')
 def search_emails():
     since = request.args.get('since')
-    user = request.args.get('user')
-    url = "https://www.googleapis.com/gmail/v1/users/%s/messages" % user
-    query = 'maxResults=1&q=after:%s' % since
-    token = json.loads(get_user_token(user))
-    access_token = token['access_token']
-    #user = session['email_addr']
-    headers = {
-        "Authorization": f"Bearer {access_token}"
-    }
-    params = {
-        "q": "after:%s" % since,
-        'maxResults': 1
-    }
-    response = requests.get(url, headers=headers, params=params)
-    messages = response.json().get('messages', [])
-    print(response.json())
-    print(messages)
-    return ""
-    try:
-        response = service.users().messages().list(userId=user, q=query).execute()
-        print(response)
-        messages = response['messages']
-        return len(messages) > 0
-    except Exception as e:
-        print(e)
-    return None
-
+    domain = session['domain']
+    return adapters[domain].search_emails(since)
 
 if __name__ == '__main__':
     app.run(debug=True, ssl_context=('/etc/letsencrypt/live/hallowelt.uk/fullchain.pem', '/etc/letsencrypt/live/hallowelt.uk/privkey.pem'))
